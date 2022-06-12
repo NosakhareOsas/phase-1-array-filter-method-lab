189801c2609836eb8599128fbaf0b7adaab5a234
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-12 13:59:20 +0100

    Test Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+    let filteredDrivers = [];
+    filteredDrivers = drivers.filter(function(driver){
+        return (driver === name || driver === name.toLowerCase());
+    });
+    return filteredDrivers;
+}
+
+const fuzzyMatch = (drivers, name) => {
+    let filteredDrivers = [];
+    filteredDrivers = drivers.filter(function(driver){
+        return (driver[0] === name[0] && driver[1] === name[1]);
+    });
+    return filteredDrivers;
+}
+
+const matchName = (drivers, paramName) => {
+    let filteredDrivers = [];
+    filteredDrivers = drivers.filter(function(driver){
+        return (driver.name === paramName);  
+    });
+    return filteredDrivers;
+}
+
